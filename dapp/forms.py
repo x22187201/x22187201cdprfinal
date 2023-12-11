af0fed15bfd60d5ed4f60131912f9479277822a4
EC2 Default User <ec2-user@ip-172-31-11-15.eu-west-1.compute.internal>
2023-12-11 12:27:51 +0000

    commiting changes

--- dapp/forms.py
+++ dapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Items
 from django.contrib.auth.models import User
+from django.core import validators
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -8,6 +9,19 @@ class ItemForm(forms.ModelForm):
         fields = ['name','description','price','file']
 
 class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField(validators=[validators.validate_email])
+    min_length = 2
+    max_length = 30
+    message_lt_min = f"Should have at least {min_length} characters."
+    message_ht_max = f"Should have at most{max_length} characters."
+    name_regex='\A[a-zA-Z]+\Z'
+    name_message='The name accepts only letters!'
+    
+    username = forms.CharField(validators=[
+    validators.MinLengthValidator(min_length, message_lt_min),
+    validators.MaxLengthValidator(max_length, message_ht_max)
+    ])
+
     password = forms.CharField(label='Password',widget=forms.PasswordInput)
     password1 = forms.CharField(label=' Confirm Password',widget=forms.PasswordInput)
     class Meta:
